9d5040c68d7c8f29467bdc90e6052391b67a414d
Junsuk Oh <a811219@gmail.com>
2021-11-05 15:33:55 +0900

    DI 설정 더 깔끔하게 업데이트

---
+++ lib/di/provider_setup.dart
@@ -0,0 +1,41 @@
+import 'package:note_app/data/data_source/local/note_data_source.dart';
+import 'package:note_app/data/repository/note_repository_impl.dart';
+import 'package:note_app/domain/use_case/add_note_use_case.dart';
+import 'package:note_app/domain/use_case/delete_note_use_case.dart';
+import 'package:note_app/domain/use_case/get_note_use_case.dart';
+import 'package:note_app/domain/use_case/get_notes_use_case.dart';
+import 'package:note_app/domain/use_case/note_use_cases.dart';
+import 'package:note_app/domain/use_case/update_note_use_case.dart';
+import 'package:note_app/presentation/add_edit_note/add_edit_note_view_model.dart';
+import 'package:note_app/presentation/notes/notes_view_model.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+import 'package:sqflite/sqflite.dart';
+
+Future<List<SingleChildWidget>> getProviders() async {
+  final db = await openDatabase(
+    'notes.db',
+    version: 1,
+    onCreate: (db, version) async {
+      await db.execute(
+          'CREATE TABLE note (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, content TEXT, color INTEGER, timestamp INTEGER)');
+    },
+  );
+
+  final dataSource = NoteDataSource(db);
+  final repository = NoteRepositoryImpl(dataSource);
+  final useCases = NoteUseCases(
+    getNotes: GetNotesUseCase(repository),
+    deleteNote: DeleteNoteUseCase(repository),
+    addNote: AddNoteUseCase(repository),
+    updateNote: UpdateNoteUseCase(repository),
+    getNote: GetNoteUseCase(repository),
+  );
+
+  return [
+    ChangeNotifierProvider<NotesViewModel>(
+        create: (context) => NotesViewModel(useCases)),
+    ChangeNotifierProvider<AddEditNoteViewModel>(
+        create: (context) => AddEditNoteViewModel(useCases)),
+  ];
+}
