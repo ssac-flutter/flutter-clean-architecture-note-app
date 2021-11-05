cd019f41b322b3b1047b17d363c628b355623884
Junsuk Oh <a811219@gmail.com>
2021-11-05 14:51:07 +0900

    노트 작성 로직 완료

---
+++ lib/presentation/add_edit_note/add_edit_note_view_model.dart
@@ -0,0 +1,59 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:note_app/domain/model/note.dart';
+import 'package:note_app/domain/use_case/note_use_cases.dart';
+import 'package:note_app/presentation/add_edit_note/add_edit_note_event.dart';
+import 'package:note_app/presentation/add_edit_note/add_edit_note_ui_event.dart';
+import 'package:note_app/ui/colors.dart';
+
+class AddEditNoteViewModel with ChangeNotifier {
+  NoteUseCases useCases;
+
+  AddEditNoteViewModel(this.useCases);
+
+  int _color = roseBud.value;
+
+  final _eventController = StreamController<AddEditNoteUiEvent>();
+  Stream<AddEditNoteUiEvent> get eventStream => _eventController.stream;
+
+  void onEvent(AddEditNoteEvent event) {
+    event.when(
+      saveNote: _saveNote,
+      changeColor: _changeColor,
+    );
+  }
+
+  void _saveNote(int? id, String title, String content) async {
+    if (title.isEmpty || content.isEmpty) {
+      _eventController.add(const AddEditNoteUiEvent.showSnackBar('제목이나 내용을 입력해 주세요'));
+      return;
+    }
+
+    if (id == null) {
+      await useCases.addNote(
+        Note(
+            title: title,
+            content: content,
+            color: _color,
+            timestamp: DateTime.now().millisecondsSinceEpoch),
+      );
+    } else {
+      await useCases.updateNote(
+        Note(
+            id: id,
+            title: title,
+            content: content,
+            color: _color,
+            timestamp: DateTime.now().millisecondsSinceEpoch),
+      );
+    }
+
+    _eventController.add(const AddEditNoteUiEvent.saveNote());
+  }
+
+  void _changeColor(int color) {
+    _color = color;
+    notifyListeners();
+  }
+}
---
+++ lib/ui/colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+const roseBud = Color(0xFFFFAB91);
+const primrose = Color(0xFFE7ED9B);
+const wisteria = Color(0xFFCF94DA);
+const skyBlue = Color(0xFF81DEEA);
+const illusion = Color(0xFFF48FB1);
+
